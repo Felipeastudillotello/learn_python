7548e53474eebb09572ede7662e5c2651cf4d739
Felipe <felang.astudillo@icloud.com>
2023-01-21 16:44:34 -0500

    files actity in python

---
+++ basic_of.py
@@ -0,0 +1,21 @@
+#estoesuntipodecomentario
+"estoesotrotipodecomentario"
+"""estoesotrotipodecomentario
+siguerescribiendo"""
+
+"tipos de datos"
+bar_int = 3
+char = "1"
+#input("escribe tu nombre")
+#ingresoNombre = input("escribe tu nombre")
+#print(ingresoNombre)
+#print(char)
+#print(bar_int)
+#suma = int(char) + bar_int
+#print(suma)
+intChar = int(char)
+resta = intChar - bar_int
+multiplication = intChar * bar_int
+divition = intChar / bar_int
+print("el valor de resta es: ",resta,"el valor de multiplication es: ",multiplication,"el valor de divition es: ",divition)
+
