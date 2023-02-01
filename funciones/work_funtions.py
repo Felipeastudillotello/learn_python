a58b625255e5a1267b2b9d17d010049adef7b2d3
Felipe <felang.astudillo@icloud.com>
2023-01-31 22:35:44 -0500

    calculo de fecha de nacimiento

--- funciones/work_funtions.py
+++ funciones/work_funtions.py
@@ -14,19 +14,19 @@ def nombre_afiliado():
    ingrese_numero_de_hijos = input("numero de hijos del afiliado",)
    array_data = [ingrese_nombre, ingrese_apellido,  ingrese_direccion, ingrese_comuna, ingrese_mail, ingrese_rut, ingrese_numero_de_hijos]
    #calculo de fecha
-   fecha_nacimiento = '30-01-12'
-   convert_fecha_nacimiento_date = datetime.datetime.strptime(fecha_nacimiento, '%d-%m-%y').date()
+   fecha_nacimiento = input("ingrese fecha nacimiento afiliado 01-01-1900",)
+   convert_fecha_nacimiento_date = datetime.datetime.strptime(fecha_nacimiento, '%d-%m-%Y').date()
    #datetime.date(2012, 1, 30)
    ano_nacimiento = convert_fecha_nacimiento_date.year
-   print(ano_nacimiento)
-   return array_data
+   edad_afiliado = 2023 - ano_nacimiento
+   print("ano de nacimiento ingresado es: ",ano_nacimiento,"\n la edad del afiliado es:", edad_afiliado)
+   return ano_nacimiento
 
 
    #invocando funcion
 nombre_afiliado()
   
 
- #queda pendiente remplazar variables para funcion de calculos  
- #
+
 
 
